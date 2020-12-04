a8719cf74e0872de99cc471071f5e57de4f6703b
ygarg704 <ygarg704@gmail.com>
2020-12-04 22:04:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ particlesJS.load('particles-js', 'particles.json', function() {
 particlesJS("particles-js", {
     "particles": {
         "number": {
-            "value": 100,
+            "value": 60,
             "density": {
                 "enable": true,
                 "value_area": 800
